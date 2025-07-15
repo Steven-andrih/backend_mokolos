fb262fb355020dfa01b711dd3dc79354d8130a0c
Steven-andrih <steevandrih@gmail.com>
2025-07-15 15:30:41 +0300

    Mis à jour des url a afficher dans le swagger.

--- config/urls.py
+++ config/urls.py
@@ -15,19 +15,19 @@ urlpatterns = [
 
     path('admin/', admin.site.urls),
 
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     path('api/register/', RegisterView.as_view(), name='register'),
     path('api/create_employer/', CreateUserByRhView.as_view(), name='create_employer'),
-    path('api/me/', MeView.as_view(), name='me'),
+    # path('api/me/', MeView.as_view(), name='me'),
 
     path('api/auth/', include('djoser.urls')),
     path('api/auth/', include('djoser.urls.jwt')),
 
     path('api/', include(router.urls)),
 
-    path("api/permission/rh/", include('permissions.urls')),
+    path("api/permissions/rh/", include('permissions.urls')),
     path("api/holyday/rh/", include('holydays.urls')),
 
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
